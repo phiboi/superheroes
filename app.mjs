51ca6dba490f1c535ce17c17d577adb8d1c4b43a
EasyParade <adam.andrews2797@gmail.com>
2019-03-26 21:52:10 -0400

    Established endpoints for oracle database transfers

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import MongoRoutes from './mongo/routes/superheroes.mjs';
+import OraRoutes from './oracle/routes/superheroes.mjs';
 import bodyParser from 'body-parser';
 
 const app = express();
@@ -9,11 +10,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const mongoUri = 'mongodb+srv://admin:admin@deebeecluster-utwpd.mongodb.net/test?retryWrites=true';
+const oraUri = '';
 mongoose
   .connect(mongoUri, {useNewUrlParser: true, dbName: 'superheroes'})
   .catch(console.error);
 
 app.use('/mongo', MongoRoutes);
+app.use('/oracle', OraRoutes);
 
 app.listen(3100, async () => {
   console.log('Listening to port 3100');
