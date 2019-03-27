401a49157a97d1d127b8418e0b359287a155c83a
EasyParade <adam.andrews2797@gmail.com>
2019-03-26 23:45:46 -0400

    Database connection successfull

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import MongoRoutes from './mongo/routes/superheroes.mjs';
+import * as oracle from './oracle/services/database.mjs';
 import OraRoutes from './oracle/routes/superheroes.mjs';
 import bodyParser from 'body-parser';
 
@@ -10,10 +11,11 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const mongoUri = 'mongodb+srv://admin:admin@deebeecluster-utwpd.mongodb.net/test?retryWrites=true';
-const oraUri = '';
 mongoose
   .connect(mongoUri, {useNewUrlParser: true, dbName: 'superheroes'})
   .catch(console.error);
+
+oracle.initialize().catch(console.error);
 
 app.use('/mongo', MongoRoutes);
 app.use('/oracle', OraRoutes);
