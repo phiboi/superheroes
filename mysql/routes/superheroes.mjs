d391941defec0f3a226bd18c21612686f618c35c
EasyParade <adam.andrews2797@gmail.com>
2019-03-28 13:45:55 -0400

    Join query

--- mysql/routes/superheroes.mjs
+++ mysql/routes/superheroes.mjs
@@ -1,32 +1,45 @@
-import express from 'express';
-import db from '../services/HeroesDB.mjs';
-import SuperHero from '../models/SuperHero.mjs';
-import SuperPower from '../models/SuperPower.mjs';
+import express from "express";
+import db from "../services/HeroesDB.mjs";
+import SuperHero from "../models/SuperHero.mjs";
+import SuperPower from "../models/SuperPower.mjs";
 
 const router = express.Router();
 /* Hero routes */
-router.get('/heroes/get', async(_, res) => {
-    SuperHero.findAll()
+router.get("/heroes/get", async (_, res) => {
+  SuperHero.findAll()
     .then(heroes => {
-        res.send(heroes);
+      res.send(heroes);
     })
-    .catch(err => console.log(err))
-
+    .catch(err => console.log(err));
 });
-router.post('/heroes/add', async({body}, res) => {
-    res.send("Not yet implemented");
+
+router.get("/heroes/join", async (_, res) => {
+  db.query(
+    "SELECT hero.name as hero, superpower.name as powers FROM Superheros AS hero JOIN SuperheroSuperpowers AS superheroPower ON hero.id = superheroPower.superheroId JOIN superpowers AS superpower ON superpower.id = superheroPower.superpowerId;"
+  )
+    .then(heroes => {
+      res.send(heroes);
+    })
+    .catch(err => console.log(err));
+  //   SELECT hero.name as hero, superpower.name as powers FROM Superheros AS hero
+  //   JOIN SuperheroSuperpowers AS superheroPower ON hero.id = superheroPower.superheroId
+  //   JOIN superpowers AS superpower ON superpower.id = superheroPower.superpowerId;
+});
+
+router.post("/heroes/add", async ({ body }, res) => {
+  res.send("Not yet implemented");
 });
 
 /* Power routes */
-router.get('/powers/get', async(_, res) => {
-    SuperPower.findAll()
+router.get("/powers/get", async (_, res) => {
+  SuperPower.findAll()
     .then(powers => {
-        res.send(powers);
+      res.send(powers);
     })
-    .catch(err => console.log(err))
+    .catch(err => console.log(err));
 });
-router.post('/powers/add', async({body}, res) => {
-    res.send("Not yet implemented");
+router.post("/powers/add", async ({ body }, res) => {
+  res.send("Not yet implemented");
 });
 
 export default router;
